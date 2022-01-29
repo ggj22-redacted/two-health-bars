e09ae69d7c32c2974f8f76b29ff278df8912deda
André Santiago <andre.mpc.santiago@gmail.com>
2022-01-29 17:11:52 +0100

    feat: added player head control

---
+++ Assets/Game/Common/Player/PlayerHeadControl.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace Game.Common.Player
+{
+    public class PlayerHeadControl : MonoBehaviour
+    {
+        [SerializeField]
+        private Transform head;
+
+        [SerializeField]
+        private Transform headPivot;
+
+        [SerializeField]
+        private float headSpeed = 1f;
+
+        [SerializeField]
+        private Camera mainCamera;
+
+        private Vector3 target;
+
+        private void Update ()
+        {
+            Vector3 cameraCenter = mainCamera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0f));
+            if (Physics.Raycast(cameraCenter, mainCamera.transform.forward, out RaycastHit hit, 100f)) {
+                target = hit.point;
+            } else {
+                target = cameraCenter + mainCamera.transform.forward * 100f;
+            }
+
+            headPivot.rotation = Quaternion.LookRotation(target - headPivot.position);
+        }
+    }
+}

410c441bb07fe7e3d4b3b63583db8242310e41f4
André Santiago <andre.mpc.santiago@gmail.com>
2022-01-29 18:38:38 +0100

    feat: added enemies following player

---
+++ Assets/Game/Common/Enemies/TransformFollower.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+namespace Game.Common.Enemies
+{
+    [RequireComponent(typeof(Collider))]
+    public class TransformFollower : MonoBehaviour
+    {
+        [SerializeField]
+        private MovementControl movementControl;
+
+        [SerializeField, Min(0)]
+        private float minDistance;
+
+        [SerializeField, Min(0)]
+        private float maxDistance;
+
+        private void OnTriggerStay (Collider other)
+        {
+            Vector3 targetPosition = other.transform.position;
+            Vector3 displacement = targetPosition - transform.position;
+            float distance = displacement.magnitude;
+            Vector3 direction = displacement / distance;
+
+            if (distance > maxDistance) {
+                movementControl.MoveTo(targetPosition + displacement - direction * minDistance);
+                return;
+            }
+
+            if (distance < minDistance)
+                movementControl.MoveTo(targetPosition - direction * minDistance);
+        }
+    }
+}

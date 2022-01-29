756f36f152f9ab8027434cc150b353703d074dda
André Santiago <andre.mpc.santiago@gmail.com>
2022-01-29 17:13:50 +0100

    refactor: removed unused member variables from PlayerHeadControl

--- Assets/Game/Common/Player/PlayerHeadControl.cs
+++ Assets/Game/Common/Player/PlayerHeadControl.cs
@@ -5,29 +5,23 @@ namespace Game.Common.Player
     public class PlayerHeadControl : MonoBehaviour
     {
         [SerializeField]
-        private Transform head;
-
-        [SerializeField]
         private Transform headPivot;
-
-        [SerializeField]
-        private float headSpeed = 1f;
 
         [SerializeField]
         private Camera mainCamera;
 
-        private Vector3 target;
+        private Vector3 _target;
 
         private void Update ()
         {
             Vector3 cameraCenter = mainCamera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0f));
             if (Physics.Raycast(cameraCenter, mainCamera.transform.forward, out RaycastHit hit, 100f)) {
-                target = hit.point;
+                _target = hit.point;
             } else {
-                target = cameraCenter + mainCamera.transform.forward * 100f;
+                _target = cameraCenter + mainCamera.transform.forward * 100f;
             }
 
-            headPivot.rotation = Quaternion.LookRotation(target - headPivot.position);
+            headPivot.rotation = Quaternion.LookRotation(_target - headPivot.position);
         }
     }
 }

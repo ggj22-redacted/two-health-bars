859e3828623dfc47a735b43e89af8332d112556f
André Santiago <andre.mpc.santiago@gmail.com>
2022-01-29 18:03:50 +0100

    feat: added base enemy navigation system

---
+++ Assets/Game/Common/Enemies/MovementControl.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Game.Common.Enemies
+{
+    [RequireComponent(typeof(NavMeshAgent))]
+    public class MovementControl : MonoBehaviour
+    {
+        private NavMeshAgent _agent;
+
+        private void Awake ()
+        {
+            _agent = GetComponent<NavMeshAgent>();
+        }
+
+        public void MoveTo (Vector3 position)
+        {
+            _agent.SetDestination(position);
+        }
+
+        public void Stop ()
+        {
+            _agent.SetDestination(_agent.nextPosition);
+        }
+
+        private void Start ()
+        {
+            MoveTo(transform.position + transform.forward * 10);
+        }
+    }
+}

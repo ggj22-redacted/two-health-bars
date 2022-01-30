047e7d53e49ea4405e186eb6937b7d70fcb29198
André Santiago <andre.mpc.santiago@gmail.com>
2022-01-30 21:49:39 +0100

    feat: added missing EnemyCounterControl

---
+++ Assets/Game/Common/UI/EnemyCounterControl.cs
@@ -0,0 +1,21 @@
+﻿using Game.Common.Enemies;
+using TMPro;
+using UnityEngine;
+using Zenject;
+
+namespace Game.Common.UI
+{
+    public class EnemyCounterControl : MonoBehaviour
+    {
+        [SerializeField]
+        private TMP_Text label;
+
+        [Inject]
+        private EnemySystem _enemySystem;
+
+        private void Update ()
+        {
+            label.text = _enemySystem.EnemiesAliveCount.ToString("D2"); // $"{_enemySystem.EnemiesAliveCount:D2}";
+        }
+    }
+}

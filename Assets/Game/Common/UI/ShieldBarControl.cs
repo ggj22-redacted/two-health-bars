8d9db6b14c0e137284ebf879de819bf3849af473
André Santiago <andre.mpc.santiago@gmail.com>
2022-01-30 12:57:40 +0100

    fix: added missing shield bar control

--- Assets/Game/Common/UI/ShieldBarControl.cs
+++ Assets/Game/Common/UI/ShieldBarControl.cs
@@ -1,9 +1,44 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
+using Zenject;
 
 namespace Game.Common.UI
 {
     public class ShieldBarControl : MonoBehaviour
     {
-        
+        [SerializeField]
+        public Transform shieldBar;
+
+        [SerializeField]
+        public TMP_Text shieldLabel;
+
+        [Inject]
+        private EntityState _entityState;
+
+        private void Start () {
+            UpdateShield(0f, _entityState.Shield);
+        }
+
+        private void OnEnable ()
+        {
+            _entityState.OnShieldChanged += UpdateShield;
+        }
+
+        private void OnDisable ()
+        {
+            _entityState.OnShieldChanged -= UpdateShield;
+        }
+
+        private void UpdateShield (float previous, float current)
+        {
+            if (shieldBar) {
+                Vector3 scale = shieldBar.localScale;
+                scale.x = current / _entityState.MaxShield;
+                shieldBar.localScale = scale;
+            }
+
+            if(shieldLabel)
+                shieldLabel.text = $"{_entityState.Shield}/{_entityState.MaxShield}";
+        }
     }
 }

6558c7fd8f165c0a0b96c8154d67d9d0f1e94104
André Santiago <andre.mpc.santiago@gmail.com>
2022-01-30 23:22:09 +0100

    feat: added round presentation behaviour

---
+++ Assets/Game/Common/UI/RoundCountControl.cs
@@ -0,0 +1,21 @@
+﻿using Game.Common.Game;
+using TMPro;
+using UnityEngine;
+using Zenject;
+
+namespace Game.Common.UI
+{
+    public class RoundCountControl : MonoBehaviour
+    {
+        [SerializeField]
+        private TMP_Text roundCountLabel;
+
+        [Inject]
+        private GameStateSystem _gameStateSystem;
+
+        private void Update ()
+        {
+            roundCountLabel.text = _gameStateSystem.CurrentRoundNumber.ToString("D2");
+        }
+    }
+}

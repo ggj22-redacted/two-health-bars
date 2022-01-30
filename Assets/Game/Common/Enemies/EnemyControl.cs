93feee23fecd36f3f263fbbcdfca048e05a72499
André Santiago <andre.mpc.santiago@gmail.com>
2022-01-30 19:41:25 +0100

    feat: added accuracy system to the enemies

--- Assets/Game/Common/Enemies/EnemyControl.cs
+++ Assets/Game/Common/Enemies/EnemyControl.cs
@@ -21,17 +21,46 @@ namespace Game.Common.Enemies
         [SerializeField, Min(0)]
         private float maxDistance;
 
+        [SerializeField]
+        private float accuracyUpdateRate;
+
+        private Vector3 _previousTargetPosition;
+
+        private Vector3 _previousPredictedPosition;
+
         private Transform _target;
 
-        private ShootingControl shootingControl;
+        private CharacterController _characterController;
 
-        private void Start() {
-            shootingControl = gameObject.GetComponent<ShootingControl>();
+        private ShootingControl _shootingControl;
+
+        private float accuracy;
+
+        private static Vector3 GetPredictedTargetPosition(Vector3 targetPosition, Vector3 shooterPosition, Vector3 targetVelocity, float projectileSpeed)
+        {
+            Vector3 displacement = targetPosition - shooterPosition;
+            float targetMoveAngle = Vector3.Angle(-displacement, targetVelocity) * Mathf.Deg2Rad;
+            //if the target is stopping or if it is impossible for the projectile to catch up with the target (Sine Formula)
+            if (targetVelocity.magnitude == 0 || targetVelocity.magnitude > projectileSpeed && Mathf.Sin(targetMoveAngle) / projectileSpeed > Mathf.Cos(targetMoveAngle) / targetVelocity.magnitude)
+            {
+                Debug.Log("Position prediction is not feasible.");
+                return targetPosition;
+            }
+            //also Sine Formula
+            float shootAngle = Mathf.Asin(Mathf.Sin(targetMoveAngle) * targetVelocity.magnitude / projectileSpeed);
+
+            return targetPosition + targetVelocity * displacement.magnitude / Mathf.Sin(Mathf.PI - targetMoveAngle - shootAngle) * Mathf.Sin(shootAngle) / targetVelocity.magnitude;
+        }
+
+        private void Start()
+        {
+            _shootingControl = gameObject.GetComponent<ShootingControl>();
         }
 
         private void OnTriggerStay (Collider other)
         {
             _target = other.transform;
+            _characterController = other.GetComponent<CharacterController>();
 
             Vector3 targetPosition = _target.position;
             Vector3 displacement = targetPosition - transform.position;
@@ -43,7 +72,7 @@ namespace Game.Common.Enemies
                 return;
             }
 
-            shootingControl.SetShooting(entityState.allowfire);
+            _shootingControl.SetShooting(entityState.allowfire);
 
             if (distance < minDistance)
                 movementControl.MoveTo(targetPosition - direction * minDistance);
@@ -53,10 +82,10 @@ namespace Game.Common.Enemies
         {
             _target = null;
 
-            shootingControl.SetShooting(false);
+            _shootingControl.SetShooting(false);
         }
 
-        private void Update ()
+        private void FixedUpdate ()
         {
             HandleRotation();
         }
@@ -66,7 +95,35 @@ namespace Game.Common.Enemies
             if (!_target)
                 return;
 
-            referenceTransform.rotation = Quaternion.LookRotation(_target.position - referenceTransform.position);
+            Vector3 targetPosition;
+            if (_characterController) {
+                targetPosition = _target.position + _target.up * _characterController.height;
+            } else {
+                targetPosition = _target.position;
+            }
+
+            Vector3 position = referenceTransform.position;
+            Vector3 displacement = targetPosition - _previousTargetPosition;
+            Vector3 velocity = displacement / Time.fixedDeltaTime;
+            Vector3 predictedTargetPosition = GetPredictedTargetPosition(targetPosition, position, velocity, entityState.ProjectileState.Speed);
+
+            Vector3 lookPosition = Vector3.LerpUnclamped(targetPosition, predictedTargetPosition, accuracy);
+            referenceTransform.rotation = Quaternion.LookRotation(lookPosition - position);
+
+            UpdateAccuracy(_previousTargetPosition, targetPosition, predictedTargetPosition);
+
+            _previousTargetPosition = targetPosition;
+            _previousPredictedPosition = predictedTargetPosition;
+        }
+
+        private void UpdateAccuracy (Vector3 startPosition, Vector3 actualPosition, Vector3 predictedPosition)
+        {
+            Vector3 displacement = actualPosition - startPosition;
+            Vector3 predictedDisplacement = predictedPosition - startPosition;
+            float dot = Vector3.Dot(displacement, predictedDisplacement);
+
+            accuracy += dot * accuracyUpdateRate;
+            accuracy = Mathf.Clamp(accuracy, 0, 1);
         }
     }
 }

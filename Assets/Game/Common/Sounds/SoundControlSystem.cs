d8fd1ef3b6cb0918d27c70471f6b12da0e4c6f58
André Santiago <andre.mpc.santiago@gmail.com>
2022-02-04 01:09:03 +0100

    feat: updated base balancing of player and enemies

--- Assets/Game/Common/Sounds/SoundControlSystem.cs
+++ Assets/Game/Common/Sounds/SoundControlSystem.cs
@@ -1,7 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Events;
 
 public class SoundControlSystem : MonoBehaviour
 {
@@ -9,7 +6,9 @@ public class SoundControlSystem : MonoBehaviour
     public struct SoundEffect
     {
         public AudioClip clip;
+
         public float volume;
+
         public float pitch;
     }
 
@@ -17,112 +16,100 @@ public class SoundControlSystem : MonoBehaviour
     public SoundEffect[] soundClips;
 
     public AudioSource playSound1;
+
     public AudioSource playSound2;
+
     private AudioClip oldestClip;
 
-    void Start()
+    private void SetAudioSettings (AudioClip clip, AudioSource source)
     {
-        
-    }
+        for (int x = 0; x < soundClips.Length; x++) {
+            if (soundClips[x].clip != clip)
+                continue;
 
-    private void SetAudioSettings(AudioClip clip, AudioSource source)
-    {
-        for (int x = 0; x < soundClips.Length; x++)
-        {
-            if (soundClips[x].clip == clip)
-            {
-                source.clip = soundClips[x].clip;
-                source.volume = soundClips[x].volume;
-                source.pitch = soundClips[x].pitch;
-            }
+            source.clip = soundClips[x].clip;
+            source.volume = soundClips[x].volume;
+            source.pitch = soundClips[x].pitch;
         }
     }
 
-    private int CheckAnyAudioSourcePlaying()
+    private int CheckAnyAudioSourcePlaying ()
     {
         int numberPlaying = 0;
 
-        if (playSound1.isPlaying)
-        {
+        if (playSound1.isPlaying) {
             numberPlaying++;
         }
 
-        if (playSound2.isPlaying)
-        {
+        if (playSound2.isPlaying) {
             numberPlaying++;
         }
 
         return numberPlaying;
     }
 
-    private AudioSource FetchAudioSource(bool playingValue)
+    private AudioSource FetchAudioSource (bool playingValue)
     {
-        if (playSound1.isPlaying == playingValue)
-        {
+        if (playSound1.isPlaying == playingValue) {
             return playSound1;
         }
 
-        if (playSound2.isPlaying == playingValue)
-        {
+        if (playSound2.isPlaying == playingValue) {
             return playSound2;
         }
 
         return null;
     }
 
-    private bool CheckClip(AudioClip clip)
+    private bool CheckClip (AudioClip clip)
     {
-        if (playSound1.clip == clip && playSound1.isPlaying)
-        {
+        if (playSound1.clip == clip && playSound1.isPlaying) {
             return true;
         }
-        if (playSound2.clip == clip && playSound2.isPlaying)
-        {
+
+        if (playSound2.clip == clip && playSound2.isPlaying) {
             return true;
         }
+
         return false;
     }
 
-    void SoundPlay(AudioClip clip)
+    private void SoundPlay (AudioClip clip)
     {
-        switch (CheckAnyAudioSourcePlaying())
-        {
+        switch (CheckAnyAudioSourcePlaying()) {
             case 0:
                 SetAudioSettings(clip, FetchAudioSource(false));
                 FetchAudioSource(false).Play();
                 break;
             case 1:
-                if (!CheckClip(clip))
-                {
+                if (!CheckClip(clip)) {
                     SetAudioSettings(clip, FetchAudioSource(false));
                     FetchAudioSource(false).Play();
                 }
                 break;
             case 2:
-                if (!CheckClip(clip))
-                {
-                    if (playSound1.clip == oldestClip)
-                    {
+                if (!CheckClip(clip)) {
+                    if (playSound1.clip == oldestClip) {
                         playSound1.Stop();
                         oldestClip = playSound2.clip;
                     }
-                    if (playSound2.clip == oldestClip)
-                    {
+
+                    if (playSound2.clip == oldestClip) {
                         playSound2.Stop();
                         oldestClip = playSound1.clip;
                     }
-                    SetAudioSettings(clip, FetchAudioSource(false));
+
+                    AudioSource source = FetchAudioSource(false);
+                    SetAudioSettings(clip, source);
                     FetchAudioSource(false).Play();
                 }
                 break;
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update ()
     {
-        if (CheckAnyAudioSourcePlaying() == 1)
-        {
+        if (CheckAnyAudioSourcePlaying() == 1) {
             oldestClip = FetchAudioSource(true).clip;
         }
     }

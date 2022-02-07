23ef7073d5a38db259f0bcf2edb02236e3390c1c
André Santiago <andre.mpc.santiago@gmail.com>
2022-02-07 22:22:46 +0100

    feat: integrated player hit animation

---
+++ Assets/Game/Common/Enemies/HitAnimationControl.cs
@@ -0,0 +1,28 @@
+﻿using Game.Common.Projectiles;
+using UnityEngine;
+
+namespace Game.Common.Enemies
+{
+    public class HitAnimationControl : MonoBehaviour, IHittable
+    {
+        private static readonly int Hit = Animator.StringToHash("Hit");
+
+        [SerializeField]
+        private Animator animator;
+
+        [SerializeField, Min(0)]
+        private float cooldown;
+
+        private float _nextAllowedTriggerMoment = -1;
+
+        public void OnHit (ProjectileState state)
+        {
+            if (Time.time < _nextAllowedTriggerMoment)
+                return;
+
+            animator.SetTrigger(Hit);
+
+            _nextAllowedTriggerMoment = Time.time + cooldown;
+        }
+    }
+}
---
+++ Assets/Game/Common/Player/PlayerHitControl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using Game.Common.Projectiles;
+using UnityEngine;
+
+namespace Game.Common.Player
+{
+    public class PlayerHitControl : MonoBehaviour, IHittable
+    {
+        [SerializeField]
+        private float invincibilityTime;
+
+        [SerializeField]
+        private CharacterController characterController;
+
+        [SerializeField]
+        private Coroutine _hitHandler;
+
+        public void OnHit (ProjectileState state)
+        {
+            _hitHandler ??= StartCoroutine(HandleHit());
+        }
+
+        private IEnumerator HandleHit ()
+        {
+            characterController.detectCollisions = false;
+
+            yield return new WaitForSeconds(invincibilityTime);
+
+            characterController.detectCollisions = true;
+
+            _hitHandler = null;
+        }
+    }
+}

7348ff41024001efcb02ad4e8cd310ef76eba871
redhellc <redhellc@gmail.com>
2022-03-06 22:57:53 +0000

    hide cross air when on countdown

---
+++ Assets/Game/Common/UI/CrossAirBehaviour.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Game.Common.Entities;
+using Zenject;
+
+namespace Game.Common.UI
+{
+    public class CrossAirBehaviour : MonoBehaviour
+    {
+        [Inject]
+        private EntityRespawner _entityRespawner;
+        private CanvasGroup thisGroup;
+        public float delayCounter;
+        private float counter;
+        // Start is called before the first frame update
+
+
+        private void Awake()
+        {
+            thisGroup = GetComponent<CanvasGroup>();
+            thisGroup.alpha = 0;
+            counter = 0;
+        }
+
+        void Start()
+        {
+        }
+
+        void HideCanvas(EntityRespawner _entityRespawner)
+        {
+            thisGroup.alpha = 0;
+            counter = delayCounter;
+        }
+
+        void ShowCanvas()
+        {
+            thisGroup.alpha = 1;
+        }
+
+        private void OnEnable()
+        {
+            _entityRespawner.OnRespawn += HideCanvas;
+        }
+
+        private void OnDisable()
+        {
+            _entityRespawner.OnRespawn -= HideCanvas;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if(counter > 0)
+            {
+                counter -= Time.deltaTime;
+                if (counter <= 0)
+                {
+                    ShowCanvas();
+                }
+            }
+        }
+    }
+}
